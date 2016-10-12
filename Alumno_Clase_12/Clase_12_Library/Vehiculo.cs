8aed50feb5a915b4d620d9d9589ff02a237a7e33
fedexj <mendozafederico@hotmail.com>
2016-10-11 23:25:59 -0300

    a

---
+++ Alumno_Clase_12/Clase_12_Library/Vehiculo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_12_Library
+{
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Yamaha, Chevrolet, Ford, Iveco, Scania, BMW
+        }
+        EMarca _marca;
+        string _patente;
+        ConsoleColor _color;
+
+        public Vehiculo(EMarca marca, string patente, ConsoleColor color)
+        {
+            this._marca=marca;
+            this._color=color;
+            this._patente = patente;
+        }
+        /// <summary>
+        /// Retornará la cantidad de ruedas del vehículo
+        /// </summary>
+        public virtual short  CantidadRuedas { get; set; }
+
+
+        protected virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendFormat("PATENTE: {0}\r\n", this._patente);
+            sb.AppendFormat("MARCA  : {0}\r\n", this._marca.ToString());
+            sb.AppendFormat("COLOR  : {0}\r\n", this._color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+       
+
+        public string MostrarVehiculo { get { return this.Mostrar(); } }
+
+        
+
+        /// <summary>
+        /// Dos vehículos son iguales si comparten la misma patente
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            bool returnAux=false;
+            if (v1._patente == v2._patente)
+            {
+                returnAux = true;
+            }
+                       
+            return returnAux;
+        }
+        /// <summary>
+        /// Dos vehículos son distintos si su patente es distinta
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+    }
+}
